1abea6bf9137fdc3263496a86ec0fc7e54864845
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-31 11:47:31 +0400

    feat(services): add Dependency Injection

--- src/main/java/com/cl/mysqldemo/services/SchoolService.java
+++ src/main/java/com/cl/mysqldemo/services/SchoolService.java
@@ -1,4 +1,13 @@
 package com.cl.mysqldemo.services;
 
+
+import com.cl.mysqldemo.repositories.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SchoolService {
+
+    @Autowired
+    SchoolRepository schoolRepository;
 }
