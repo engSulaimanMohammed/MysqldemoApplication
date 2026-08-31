a75830110e6774bcbe1c7a96e00e9870c4e8dea0
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-31 11:55:53 +0400

    feat(school): add create school method

--- src/main/java/com/cl/mysqldemo/controllers/SchoolController.java
+++ src/main/java/com/cl/mysqldemo/controllers/SchoolController.java
@@ -1,10 +1,13 @@
 package com.cl.mysqldemo.controllers;
 
 
+import com.cl.mysqldemo.entities.School;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("School")
