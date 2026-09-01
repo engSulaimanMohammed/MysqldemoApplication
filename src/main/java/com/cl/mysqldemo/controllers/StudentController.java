0e0d0cdc6c811d266bc334090e6e94254df4f419
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-09-01 09:49:50 +0400

    Add request objects pacake

--- src/main/java/com/cl/mysqldemo/controllers/StudentController.java
+++ src/main/java/com/cl/mysqldemo/controllers/StudentController.java
@@ -1,4 +1,18 @@
 package com.cl.mysqldemo.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Mapping")
 public class StudentController {
+
+    public StudentController() {
+    }
+
+
+    public Long addStudent() {
+
+    }
 }
