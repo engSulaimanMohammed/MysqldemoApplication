3c47871ec5d4b684c665f9bcab73a7179089e441
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-09-01 09:44:20 +0400

    feat(contrllers): create Student Controller class

---
+++ src/main/java/com/cl/mysqldemo/controllers/StudentController.java
@@ -0,0 +1,4 @@
+package com.cl.mysqldemo.controllers;
+
+public class StudentController {
+}
--- src/main/java/com/cl/mysqldemo/services/StudentService.java
+++ src/main/java/com/cl/mysqldemo/services/StudentService.java
@@ -1,4 +1,8 @@
 package com.cl.mysqldemo.services;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
 }
