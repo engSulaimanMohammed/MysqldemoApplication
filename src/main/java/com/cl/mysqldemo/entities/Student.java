e3d1ad02bde939a849fde998cf53399f0c9e8488
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-09-01 09:22:45 +0400

    feat(student): add Student entity

--- src/main/java/com/cl/mysqldemo/entities/Student.java
+++ src/main/java/com/cl/mysqldemo/entities/Student.java
@@ -1,4 +1,33 @@
 package com.cl.mysqldemo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+
+
+@Entity
+@Getter
+@Setter
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+    private String name;
+    private String major;
+    private String gender;
+    private String phoneNumber;
+    private String parentName;
+    private Boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
+
 }
