2bd3eae97af46d3f3b01e2ab3959b65b9ca42e43
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-09-02 09:36:09 +0400

    feat(student): add StudentDTO conversion method

--- src/main/java/com/cl/mysqldemo/dto/StudentDTO.java
+++ src/main/java/com/cl/mysqldemo/dto/StudentDTO.java
@@ -1,6 +1,7 @@
 package com.cl.mysqldemo.dto;
 
 
+import com.cl.mysqldemo.entities.Student;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +17,17 @@ public class StudentDTO {
 
 
 
-    public static StudentDTO
+    public static StudentDTO convertToDTO(Student entity) {
+
+        StudentDTO dto = StudentDTO.builder()
+                .studentId(entity.getId())
+                .studentName(entity.getName())
+                .schoolLocation(entity.getSchool().getLocation())
+                .build();
+        return dto;
+    }
+
+
 
 
 
